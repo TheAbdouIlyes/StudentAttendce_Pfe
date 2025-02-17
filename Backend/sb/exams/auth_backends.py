91f4b1b6f9a13444ef72942db53bf0a61fe8d327
adem_chemlal <ishak33.ir@gmail.com>
2025-02-17 21:38:35 +0100

    Update auth_backends.py

--- Backend/sb/exams/auth_backends.py
+++ Backend/sb/exams/auth_backends.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.models import User
-from .models import AuthTable,teacher
+from .models import AuthTable,teacher,Student
 
 class EmailPhoneAuthBackend(BaseBackend):
     def authenticate(self, request, address=None, phone_number=None):
@@ -16,17 +16,34 @@ class EmailPhoneAuthBackend(BaseBackend):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-# class matriculemailAuthBackend(BaseBackend):
-#     def authenticate(self, request, matricul=None, secret_number=None):
-#         try:
-#            # user = User.objects.get(email=email)
-#             teacher = teacher.objects.get(matricul=matricul, secret_number=secret_number)
-#             return teacher.user  # Authentication successful
-#         except (User.DoesNotExist, teacher.DoesNotExist):
-#             return None  # Authentication failed
+class matriculsecretAuthBackend(BaseBackend):
+    def authenticate(self, request, matricul=None, secret_number=None):
+        try:
+           # user = User.objects.get(email=email)
+            Teacher =teacher.objects.get(matricul=matricul, secret_number=secret_number)
+            return Teacher.user  # Authentication successful
+        except (User.DoesNotExist, teacher.DoesNotExist):
+            return None  # Authentication failed
         
-#     def get_user(self, user_id):
-#         try:
-#             return User.objects.get(pk=user_id)
-#         except User.DoesNotExist:
-#             return None
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        
+
+from .models import Student
+
+class matriculsecretAuthBackendetud(BaseBackend):
+    def authenticate(self, request, matricul=None, roll_number=None):
+        try:
+           Student1 =Student.objects.get(matricul=matricul, roll_number=roll_number)
+           return Student1.Name # Authentication successful
+        except (User.DoesNotExist, Student.DoesNotExist):
+            return None  # Authentication failed
+        
+    def get_user(self, Name_id):
+        try:
+            return User.objects.get(pk=Name_id)
+        except User.DoesNotExist:
+            return None
